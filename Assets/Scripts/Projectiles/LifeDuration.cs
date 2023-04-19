b9b294f7439fbe4687b62f446ba1d12ef3cb2f3f
Llwnr <noregrets.llwnr@gmail.com>
2023-04-19 20:23:10 +0545

    bullet

---
+++ Assets/Scripts/Projectiles/LifeDuration.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeDuration : MonoBehaviour
+{
+    [SerializeField]private float duration;
+    // Update is called once per frame
+    void Update()
+    {
+        duration -= Time.deltaTime;
+        if(duration < 0){
+            Destroy(gameObject);
+        }
+    }
+}
