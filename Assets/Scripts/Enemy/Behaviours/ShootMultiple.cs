2c9ec435db72bf57f562b6e3c53cd85b3e7c3aec
Llwnr <noregrets.llwnr@gmail.com>
2023-05-24 19:23:55 +0545

    thanks

--- Assets/Scripts/Enemy/Behaviours/ShootMultiple.cs
+++ Assets/Scripts/Enemy/Behaviours/ShootMultiple.cs
@@ -11,9 +11,16 @@ public class ShootMultiple : ActionNode
     public float spreadAngle;
     public int numOfBullets;
 
+    public float throwDuration;
+    private float durationCount;
+
+    private int i;
+
     private Vector2 targetDir;
     protected override void OnStart() {
         targetDir = GameObject.FindWithTag("Player").transform.position - context.transform.position;
+        ResetThrowDuration();
+        i = 0;
     }
 
     protected override void OnStop() {
@@ -23,17 +30,30 @@ public class ShootMultiple : ActionNode
         int indexDivisor = numOfBullets/2;
         Vector2 spreadDir = Vector2.zero;
         float newSpreadAngle;
-        for(int i=0; i<numOfBullets; i++){
-            //Find angle to player
-            float playerTargetAngle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+        //Only throw one bullet at a time even with spread
+        durationCount -= Time.deltaTime;
+        //Find angle to player
+        float playerTargetAngle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+        if(durationCount < 0){
+            ResetThrowDuration();
             //Spread it
             newSpreadAngle = playerTargetAngle + spreadAngle * (i-indexDivisor);
-
             spreadDir = new Vector2(Mathf.Cos(Mathf.Deg2Rad * newSpreadAngle), Mathf.Sin(Mathf.Deg2Rad * newSpreadAngle));
-            GameObject newBullet = GameObject.Instantiate(bullet, context.transform.position, Quaternion.identity);
-            Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
-            rb.AddForce((spreadDir)*shootForce, ForceMode2D.Impulse);
+            ThrowBullet(spreadDir);
+            i++;
         }
-        return State.Success;
+
+        if(i >= numOfBullets) return State.Success;
+        else return State.Running;
+    }
+
+    void ResetThrowDuration(){
+        durationCount = throwDuration;
+    }
+
+    void ThrowBullet(Vector2 dir){
+        GameObject newBullet = GameObject.Instantiate(bullet, context.transform.position, Quaternion.identity);
+        Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(dir*shootForce, ForceMode2D.Impulse);
     }
 }
