3dd9439c4bbf0d4cbcdb693ff3739b4843d99307
Llwnr <noregrets.llwnr@gmail.com>
2023-07-20 12:06:27 +0545

    Revert "sfx"
    This reverts commit bd7babea6228ebd04ecd6da35077c08aba6501f3.

--- Assets/Scripts/Camera/CameraFocus.cs
+++ Assets/Scripts/Camera/CameraFocus.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 
-public class CameraFocus : MonoBehaviour, IDashObserver
+public class CameraFocus : MonoBehaviour
 {
     public static CameraFocus instance {get; private set;}
 
@@ -16,13 +14,6 @@ public class CameraFocus : MonoBehaviour, IDashObserver
     [SerializeField]private float speed;
 
     private bool toZoom = false;
-
-    //For chromatic aberration
-    [SerializeField]private Volume postProcessVolume;
-    private ChromaticAberration chromaticAberration;
-    private LensDistortion lensDistortion;
-
-    private BallDash ballDash;
 
     private void Awake() {
         if(!instance){
@@ -37,20 +28,6 @@ public class CameraFocus : MonoBehaviour, IDashObserver
     {
         vCam = GetComponent<CinemachineVirtualCamera>();
         origOrthoSize = vCam.m_Lens.OrthographicSize;
-        if(postProcessVolume.profile.TryGet<ChromaticAberration>(out chromaticAberration)){
-            Debug.Log("success");
-        }
-        if(postProcessVolume.profile.TryGet<LensDistortion>(out lensDistortion)){
-
-        }
-
-        //For camera to zoom while dashing
-        ballDash = GameObject.FindWithTag("Player").GetComponent<BallDash>();
-        ballDash.AddDashObserver(this);
-    }
-
-    private void OnDestroy() {
-        ballDash.RemoveDashObserver(this);
     }
 
     // Update is called once per frame
@@ -67,15 +44,5 @@ public class CameraFocus : MonoBehaviour, IDashObserver
         toZoom = value;
     }
 
-    public void DashStart()
-    {
-        chromaticAberration.intensity.value = 0.7f;
-        lensDistortion.intensity.value = -0.2f;
-    }
 
-    public void DashEnd()
-    {
-        chromaticAberration.intensity.value = 0;
-        lensDistortion.intensity.value = 0;
-    }
 }
